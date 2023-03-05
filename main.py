6444639ab8c84c5c0af346c7cc5b1024cb200602
Nikita Sosyuk <nmsosyuk@avito.ru>
2023-03-05 21:29:33 +0300

    homework 2

--- main.py
+++ main.py
@@ -1,13 +1,98 @@
+from crawler import Crawler
+
+import requests
 from bs4 import BeautifulSoup
-from bs4.element import Comment
-
+import re
 import os
-import shutil
-import urllib.request
+import pymorphy2
+import nltk
+from stop_words import get_stop_words
 
-from source.Crawler import Crawler
+nltk.download('punkt')
+
+# Инициализируем анализатор морфологии
+morph = pymorphy2.MorphAnalyzer()
+
+stop_words = get_stop_words('ru')
+
+
+def get_contents():
+    # Создаем директорию для файлов, если она не существует
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+    # Список страниц для загрузки
+    urls = []
+
+    # Создаем файл index.txt
+    with open('index.txt', 'w', encoding='utf-8') as f:
+        for i in range(1, 101):
+            urls.append(f"https://habr.com/ru/post/{i}")
+        for i, url in enumerate(urls):
+            # Получаем содержимое страницы
+            response = requests.get(url)
+            content = response.content
+
+            # Парсим содержимое страницы и сохраняем его в файл
+            soup = BeautifulSoup(content, 'html.parser')
+            text = soup.get_text().lower()
+            text = re.sub(r'\s+', ' ', text)  # удаляем лишние пробелы и переносы строк
+            filename = f'output/{i + 1}.txt'
+            with open(filename, 'w', encoding='utf-8') as page_file:
+                page_file.write(text)
+
+            # Записываем номер файла и ссылку на страницу в файл index.txt
+            f.write(f'{i + 1}. {url}\n')
+
+
+# Функция для получения списка токенов из файла
+def get_tokens(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        text = f.read()
+        # Удаляем лишние символы
+        text = re.sub(r'\W+', ' ', text)
+        # Токенизируем текст
+        tokens = nltk.word_tokenize(text)
+        # Отфильтровываем стоп-слова и "мусор"
+        tokens = [token for token in tokens if token not in stop_words and re.match(r'^[а-яА-Я]+$', token) and not any(
+            char.isdigit() for char in token)]
+        # Лемматизируем токены
+        # lemmas = [morph.parse(token)[0].normal_form for token in tokens]
+        # Оставляем только уникальные леммы
+        unique_lemmas = set(tokens)
+        return list(unique_lemmas)
+
+
+def get_lemmas():
+    # Создаем пустой список токенов
+    tokens = []
+
+    # Перебираем все файлы в директории output
+    for filename in os.listdir('output'):
+        # Получаем список токенов из файла
+        file_tokens = get_tokens('output/' + filename)
+        # Добавляем токены в общий список
+        tokens += file_tokens
+
+    tokens = set(tokens)
+    # Группируем токены по леммам
+    lemmas = {}
+    for token in tokens:
+        lemma = morph.parse(token)[0].normal_form
+        if lemma in lemmas:
+            lemmas[lemma].append(token)
+        else:
+            lemmas[lemma] = [token]
+
+    with open('tokens.txt', 'w', encoding='utf-8') as f:
+        for token in sorted(tokens):
+            f.write(token + ' ')
+
+    with open('grouped_tokens.txt', 'w', encoding='utf-8') as f:
+        for lemma in sorted(lemmas.keys()):
+            f.write(lemma + ': ' + ', '.join(lemmas[lemma]) + '\n')
+
 
 if __name__ == '__main__':
-    base_url = "https://www.crummy.com/software/BeautifulSoup/bs4/doc.ru/bs4ru.html"
-    spider = Crawler(base_url, nested_link_class)
-    spider.start_parsing()
+    get_contents()
+    # get_lemmas()
--- source/Crawler.py
+++ source/Crawler.py
@@ -5,16 +5,33 @@ import os
 import shutil
 import urllib.request
 
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def get_html(url):
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+
 class Crawler:
 
     def __init__(self,
                  base_url,
+                 nested_link_class,
                  max_pages_count=100,
+                 min_words_count=500,
                  output_directory="output/",
                  output_filename="index.txt"):
         self.__base_url = base_url
-        self.__nested_link_class = "reference internal"
+        self.__nested_link_class = nested_link_class
         self.__max_pages_count = max_pages_count
+        self.__min_words_count = min_words_count
         self.__output_directory = output_directory
         self.__output_filename = output_filename
         self.__current_page_index = 0
@@ -27,16 +44,17 @@ class Crawler:
 
         while self.__queue and self.__current_page_index < self.__max_pages_count:
             url = self.__queue.pop()
-            html = self.__get_html(url)
+            html = get_html(url)
             soup = BeautifulSoup(html, 'html.parser')
 
             print('Start handling %d %s ...' % (self.__current_page_index, url))
 
-            self.__save_html(self.__current_page_index, url, html)
-            self.__parsed_urls.add(url)
-            self.__current_page_index += 1
-            nested_links = list(filter(self.__is_handled, self.__get_nested_links(soup)))
-            self.__queue.extend(nested_links)
+            if self.__check_text_size(soup):
+                self.__save_html(self.__current_page_index, url, html)
+                self.__parsed_urls.add(url)
+                self.__current_page_index += 1
+                nested_links = list(filter(self.__is_handled, self.__get_nested_links(soup)))
+                self.__queue.extend(nested_links)
 
         print("Done!")
 
@@ -57,6 +75,10 @@ class Crawler:
         links = list(set([self.__base_url + item['href'] for item in internal_references]))
         return links
 
+    def __check_text_size(self, soup):
+        size = len(soup.text.split())
+        return size >= self.__min_words_count
+
     def __save_html(self, index, url, html):
         html_filename_path = self.__output_directory + str(index) + ".txt"
         html_file = open(html_filename_path, "wb")
@@ -68,14 +90,3 @@ class Crawler:
         with open(output_filename_path, 'a') as file:
             line = str(index) + " – " + url + "\n"
             file.write(line)
-
-    def __tag_visible(self, element):
-        if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
-            return False
-        if isinstance(element, Comment):
-            return False
-        return True
-
-    def __get_html(self, current_url):
-        response = urllib.request.urlopen(current_url)
-        return response.read()
